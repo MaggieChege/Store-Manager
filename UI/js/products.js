8321f39e7ff7f819c4461e6afa48632a779a2217
MaggieChege <shikooh.maggie1@gmail.com>
2018-11-12 22:07:34 +0300

    [ch-161890998]changed folder structure

--- UI/js/products.js
+++ UI/js/products.js
@@ -1,45 +1,45 @@
-// get all products
-function getToken(){
-    token = localStorage.getItem('token');
-    return token;
-}
-token = getToken()
-window.onload = function products(){
-	let products_url = 'http://127.0.0.1:5000/api/v2/products';
+// // get all products
+// function getToken(){
+//     token = localStorage.getItem('token');
+//     return token;
+// }
+// token = getToken()
+// window.onload = function products(){
+// 	let products_url = 'http://127.0.0.1:5000/api/v2/products';
 
 
-  fetch(products_url,{
-    method:"GET",
-    Headers: {
-			'Accept':'application/json',
-			'Content-type':'application/json',
-      "Authorization":'Bearer' + token
-    },
-    })
+//   fetch(products_url,{
+//     method:"GET",
+//     Headers: {
+// 			'Accept':'application/json',
+// 			'Content-type':'application/json',
+//       "Authorization":'Bearer' + token
+//     },
+//     })
 
-  .then(respose => respose.json())
-  .then((data) =>{
-    let products = data.products;
-    let output = '<h1>products</h1>'
-    // let product_table = document.getElementById('my_products');
-    //
-    //
-    //       product_table.innerHTML = th
-          data.forEach(function(products){
-            output +=`
-            <h5>${product.productid}</h5>
+//   .then(respose => respose.json())
+//   .then((data) =>{
+//     let products = data.products;
+//     let output = '<h1>products</h1>'
+//     // let product_table = document.getElementById('my_products');
+//     //
+//     //
+//     //       product_table.innerHTML = th
+//           data.forEach(function(products){
+//             output +=`
+//             <h5>${product.productid}</h5>
 
 
-            `
+//             `
 
-                  // '<td>'+product.productid+'</td>'+
-                  // '<td>'+product.product_name+'</td>'+
-                  // '<td>'+product.price+'</td>'+
-                  // '<td>'+product.product_quantity+'</td>'+
-                  '<td><a href="edit_product.html"><button id="edit_product">Edit</button></a>'+
-                  '&nbsp;&nbsp;'+
-                  '<button id="delete_product">Delete</button></td>'+
-                  '</tr>';
-          })
-  })
-}
+//                   // '<td>'+product.productid+'</td>'+
+//                   // '<td>'+product.product_name+'</td>'+
+//                   // '<td>'+product.price+'</td>'+
+//                   // '<td>'+product.product_quantity+'</td>'+
+//                   '<td><a href="edit_product.html"><button id="edit_product">Edit</button></a>'+
+//                   '&nbsp;&nbsp;'+
+//                   '<button id="delete_product">Delete</button></td>'+
+//                   '</tr>';
+//           })
+//   })
+// }
