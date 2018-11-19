688b41bb2b10e7aba39b87dabb65fc9fcc5d5bb7
MaggieChege <shikooh.maggie1@gmail.com>
2018-11-19 11:22:37 +0300

    [ft-161890016]made changes to add product

--- UI/js/products.js
+++ UI/js/products.js
@@ -1,15 +1,16 @@
 // get all products
-function getToken(){
-    token = localStorage.getItem('token');
-    return token;
+
+function setToken(result){
+   return localStorage.setItem('access_token', result.body.access_token)
 }
 
-// function edit(){
-// 	let updateform = document.getElementById("form-popup");
-// 	updateform.style.display = "block";
-// 	}
-token = getToken()
-window.onload = function products(){
+function getToken(){
+    access_token = localStorage.getItem('access_token');
+    return access_token;
+}
+
+access_token = getToken()
+// window.onload = function products(){
 	let products_url = 'http://127.0.0.1:5000/api/v2/products';
 
 
@@ -18,7 +19,7 @@ window.onload = function products(){
     Headers: {
 			'Accept':'application/json',
 			'Content-type':'application/json',
-			"Authorization":'Bearer' + token
+			"Authorization":'Bearer' + access_token
     },
     })
 
@@ -27,149 +28,65 @@ window.onload = function products(){
   	console.log(data);
   	let products = data.products;
   	let product_table  = document.getElementById('my_products');
-            th = `
-                <tr>
-                    <th>Product ID</th>
-                    <th>Product Name</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Category</th>
-                    <th>Action</td>
-                </tr>
+  	let result = '';
+  	products.forEach(product =>{
+  		result +=`
+            	<table>
+                              
+                    <tr>
+                    <td><img src="img/AIR JORDAN 11 HEIRESS.jpg"height="150" width="150">
+                    <h4>product_name:${product.product_name}</h4>
+                    <h4>category:${product.category}</h4>
+                    <h4>quantity:${product.quantity}</h4>
+                    <h4>Kshs:${product.price}</h4>
+                    <button onclick="popup()" value="cart" class="btn-cart" style="margin-bottom: 100px;"">Shopping Cart </button>
+                    
+                    </td>
+                    </tr>
+               
+                
+            </table>
+
             `
-
-
-            product_table.innerHTML = th
-            products.forEach(product =>{
-            	'<td>'+product.product_id+'</td>'+
-                    '<td>'+product.product_name+'</td>'+
-                    '<td>'+product.price+'</td>'+
-                    '<td>'+product.category+'</td>'+
-                    '<td>'+product.product_quantity+'</td>'+
-                    '<td><a href="edit_product.html"><button id="edit_product">Edit</button></a>'+
-                    '&nbsp;&nbsp;'+
-                    '<button id="delete_product">Delete</button></td>'+
-                    '</tr>';
+            product_table.innerHTML = result;
             })
+  	        localStorage.setItem('products', JSON.stringify(data.products));
 
   	})
-  }
+  // }
+// 
+function popup(){
+  document.getElementById("myForm").style.display = "block";
+}
+document.getElementById('sale').addEventListener('click',sell_product)
+function sell_product(){
+  let product_name = document.getElementById('product_name').value;  
+  let quantity = document.getElementById('quantity').value; 
+  let attendant = document.getElementById('attendant').value;
+  fetch('http://127.0.0.1:5000/api/v2/sales',{
+    method: 'POST',
+    headers:{
+      'Accept':'application/json',
+      'Content-type':'application/json',
+      "Authorization":'Bearer' + access_token
+    },body:JSON.stringify({product_name:product_name,quantity:quantity,attendant:attendant})
+  })
+  .then(result => result.json().then(data => ({status: result.status, body: data})))
+  .then(result => {
+     console.log(result)
+     console.log(result.body.data)
+     if(result.status == 201){
+       document.getElementById('success_added').innerHTML = result.body.message;
+       window.location.reload();
+     }else if(result.status == 500){
+     alert("Please Login again");
+     window.setTimeout("location.href = 'index.html';",2000)}
+     else{
+       alert(result.body.message);
+     }
 
-
-// add product
-
-
-
-function products(){
-	let products_url = 'http://127.0.0.1:5000/api/v2/products';
-	let product_name = document.getElementById("product_name").value
-	let category = document.getElementById("category").value
-	let quantity = document.getElementById("quantity").value
-	let price = document.getElementById("price").value
-
-
-
-	let data = {product_name: product_name,
-				category: category,
-				quantity: quantity,
-				price: price}
-
-
-  fetch(products_url,{
-    method:"POST",
-    body: JSON.stringify(data),
-    headers: {
-			'Accept':'application/json',
-			'Content-type':'application/json',
-			"Authorization":'Bearer' + token
-    }
-    }).then(res => res.json())
-	.then(response => {
-		console.log(response);
-		if (response["message"]!="Product added successfully"){
-			return alert(JSON.stringify(response["message"]));
-		}else{
-			document.getElementById("product_name").value ="";
-	    	document.getElementById("quantity").value = "";
-	    	document.getElementById("price").value = "";
-	    	document.getElementById("product_category").value ="";
-			return alert(response["message"]);
-		}
-	})
-	.catch(error => console.error('Error:', error));
+})
 }
 
 
-// update product
 
-function Editproducts(){
-	let products_url = 'http://127.0.0.1:5000/api/v2/products';
-	let product_name = document.getElementById("product_name").value
-	let category = document.getElementById("category").value
-	let quantity = document.getElementById("quantity").value
-	let price = document.getElementById("price").value
-
-
-
-	let data = {product_name: product_name,
-				category: category,
-				quantity: quantity,
-				price: price}
-
-
-  fetch(products_url,{
-    method:"PUT",
-    body: JSON.stringify(data),
-    headers: {
-			'Accept':'application/json',
-			'Content-type':'application/json',
-			"Authorization":'Bearer' + token
-    }
-    }).then(res => res.json())
-	.then(response => {
-		console.log(response);
-		if (response["message"]!="Product added successfully"){
-			return alert(JSON.stringify(response["message"]));
-		}else{
-			alert(response["message"]);
-		}
-	})
-	.catch(error => console.error('Error:', error));
-}
-
-
-// delete product
-
-function Deleteproducts(){
-	let products_url = 'http://127.0.0.1:5000/api/v2/products';
-	let product_name = document.getElementById("product_name").value
-	let category = document.getElementById("category").value
-	let quantity = document.getElementById("quantity").value
-	let del = confirm("Delete");
-	if (del == false){
-
-	}
-
-
-
-
-
-  fetch(products_url,{
-    method:"DELETE",
-    body: JSON.stringify(data),
-    headers: {
-			'Accept':'application/json',
-			'Content-type':'application/json',
-			"Authorization":'Bearer' + token
-    }
-    }).then(res => res.json())
-	.then(response => {
-		console.log(response);
-		if (response["message"]=="Product deleted successfully"){
-			alert(response["message"]):
-		}else {
-			return alert(response["message"]);
-		}
-	})
-	.catch(error => console.error('Error:', error));
-}
